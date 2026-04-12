1f9d29debe7864e9c9ba17e7c45a4c73587e2335
kaoru-528 <origami.kaoru@gmail.com>
2026-04-12 11:26:00 +0900

    add:コマンド履歴の検索機能を追加

---
+++ .config/zsh/history.zsh
@@ -0,0 +1,19 @@
+# History設定
+HISTFILE=~/.zsh_history
+HISTSIZE=100000
+SAVEHIST=100000
+setopt share_history
+setopt hist_ignore_dups
+setopt hist_ignore_all_dups
+setopt hist_ignore_space
+setopt hist_reduce_blanks
+setopt print_eight_bit
+
+# fzf history
+function fzf-select-history() {
+    BUFFER=$(history -n -r 1 | fzf --query "$LBUFFER" --reverse)
+    CURSOR=$#BUFFER
+    zle reset-prompt
+}
+zle -N fzf-select-history
+bindkey '^r' fzf-select-history
